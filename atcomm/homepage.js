ae2a5f781536efffc1e6e109384f8e4c6add79d9
Kevin Booth <kdbooth97@gmail.com>
2018-02-06 13:24:19 -0500

    dd
    dd

---
+++ atcomm/homepage.js
@@ -0,0 +1,12 @@
+function openMobileMenu() {
+  $('.mobile-menu').click(function() {
+    $(this).find(ul.child).show(200);
+  }, function () {
+    $(this).find(ul.child).hide();
+  });
+}
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
